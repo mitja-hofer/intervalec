03f9b0090e77c17aacac7b70cff1710a06e180a5
ziga-mali <zm9728@student.uni-lj.si>
2022-06-04 17:31:55 +0200

    Added .xml file for new intervals

--- app/src/main/java/com/tnuv/intervalec/AddIntervalActivity.java
+++ app/src/main/java/com/tnuv/intervalec/AddIntervalActivity.java
@@ -2,7 +2,30 @@ package com.tnuv.intervalec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import java.util.Locale;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class AddIntervalActivity extends AppCompatActivity {
     // https://stackoverflow.com/questions/7145606/how-do-you-save-store-objects-in-sharedpreferences-on-android
     // see example of adding new interval in SelectIntervalActivity
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_interval);
+    }
 }
---
+++ app/src/main/java/com/tnuv/intervalec/AddProgramActivity.java
@@ -0,0 +1,14 @@
+package com.tnuv.intervalec;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddProgramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_program);
+    }
+}
